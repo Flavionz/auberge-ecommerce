f06125f5d80513aed051884bffdee2bd7cf582a7
Flavionz <flavionzeus@gmail.com>
2026-05-11 10:36:49 +0200

    fix: add casasteph.fr to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     process.env.CLIENT_URL,
+    'https://casasteph.fr',
+    'https://www.casasteph.fr',
 ].filter(Boolean);
 
 app.use(helmet({
